d5689e23ab7a429e77ef85805c25af6b8a66d786
Alex Brazier <aejbrazier.apps@gmail.com>
2018-06-16 13:32:40 +0100

    feat: Change colour of question to show how correct

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -13,6 +13,22 @@ export default class Question extends Component {
     })).isRequired,
   }
 
+  /**
+   * Get background colour of question box based on how correct
+   * @param {number} correct Number of correct answers
+   * @param {number} total Total number of answers
+   */
+  static backgroundColour(correct, total) {
+    if (correct === total) {
+      return 'linear-gradient(to bottom, rgba(71, 228, 193), rgba(71, 205, 221))';
+    }
+    const greenMin = [145, 59];
+    const greenRange = [85, 151];
+    const ratio = correct / total;
+    const green = greenMin.map((g, i) => g + greenRange[i] * ratio);
+    return `linear-gradient(to bottom, rgba(250, ${green[0]}, 97), rgba(247, ${green[1]}, 28))`;
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -24,24 +40,27 @@ export default class Question extends Component {
     const { option, answer } = e.target.dataset;
     const selected = [...this.state.selected];
     selected[Number(answer)] = Number(option);
-    const correct = this.props.answers.every((a, i) => selected[i] === a.correct);
-    this.setState({ selected, correct });
+    this.setState({ selected });
   }
 
   render() {
+    const { selected } = this.state;
+    const numberCorrect = this.props.answers.filter((a, i) => selected[i] === a.correct).length;
+    const correct = numberCorrect === this.props.answers.length;
+    const background = Question.backgroundColour(numberCorrect, this.props.answers.length);
     return (
-      <div className={`Question${this.state.correct ? ' correct' : ''}`}>
+      <div className={`Question${correct ? ' correct' : ''}`} style={{ background }}>
         <p className="question">{this.props.question}</p>
         {this.props.answers.map((answer, i) => (
           <Answer
             key={i}
             index={i}
             options={answer.options}
-            selected={this.state.selected[i]}
+            selected={selected[i]}
             toggle={this.toggle}
           />
         ))}
-        <p className="result">The answer is {this.state.correct ? 'correct' : 'incorrect'}</p>
+        <p className="result">The answer is {correct ? 'correct' : 'incorrect'}</p>
       </div>
     );
   }
