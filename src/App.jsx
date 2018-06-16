57457d5f37db5cc84e69fe511389815188e3f0ce
Alex Brazier <aejbrazier.apps@gmail.com>
2018-06-16 12:48:29 +0100

    feat: Randomise questions

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Footer from './components/Footer';
 import questions from './data/questions.json';
 import './App.scss';
 
+const question = Math.floor(Math.random() * questions.length);
 const App = props => (
   <div className="App">
     <header>
       <h1 className="title">Questions</h1>
     </header>
     <section className="content">
-      <Question {...questions[0]} />
+      <Question {...questions[question]} />
     </section>
     <Footer />
   </div>
